001bac12931bbec4cf9748206701c3320a95b42b
Abhishek Singh <abhisinghjnv143@gmail.com>
2025-04-09 00:49:19 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, send_file
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return send_file('index.html')
+
+@app.route('/data')
+def data():
+    # Make sure your CSV file is named exactly 'Scores.csv'
+    df = pd.read_csv('Scores.csv')
+    students = []
+
+    for _, row in df.iterrows():
+        name = row['Name']
+        scores = row.drop('Name').to_dict()
+        students.append({'name': name, 'scores': scores})
+
+    return jsonify(students)
+
+if __name__ == '__main__':
+    app.run(debug=True)
